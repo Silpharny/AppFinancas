54688a4b355a8c9fde73508192d9c9b5a5d74a54
Silpharny <silpharny@gmail.com>
2024-07-01 19:13:25 -0300

    Criando aplicativo finanças

---
+++ src/pages/SignIn/index.js
@@ -0,0 +1,45 @@
+import React from "react";
+
+import { Platform } from "react-native";
+
+import {
+  Background,
+  Container,
+  Logo,
+  AreaInput,
+  Input,
+  SubmitButton,
+  SubmitText,
+  Link,
+  LinkText,
+} from "./styles";
+
+import { useNavigation } from "@react-navigation/native";
+
+export default function SignIn() {
+  const navigation = useNavigation();
+
+  return (
+    <Background>
+      <Container behavior={Platform.OS === "ios" ? "padding" : ""} enabled>
+        <Logo source={require("../../assets/logo.png")} />
+
+        <AreaInput>
+          <Input placeholder="Seu Email" />
+        </AreaInput>
+
+        <AreaInput>
+          <Input placeholder="Sua Senha" />
+        </AreaInput>
+
+        <SubmitButton activeOpacity={0.8}>
+          <SubmitText>Acessar</SubmitText>
+        </SubmitButton>
+
+        <Link onPress={() => navigation.navigate("SignUp")}>
+          <LinkText>Criar uma conta</LinkText>
+        </Link>
+      </Container>
+    </Background>
+  );
+}
